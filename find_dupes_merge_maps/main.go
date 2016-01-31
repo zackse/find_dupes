3cb33e6999ea324905c0978e297c71584aabe73a
Evan Zacks <zackse@gmail.com>
2016-01-31 15:26:05 -0500

    group dupes by size first, then hash matches to find dupes

--- find_dupes_merge_maps/main.go
+++ find_dupes_merge_maps/main.go
@@ -19,25 +19,36 @@ var Excludes = map[string]bool{
 type FileDesc struct {
 	path  string
 	mtime int64
-	hash  string
+	size  int64
 }
 
 func generateFileDesc(id int, path string) (FileDesc, error) {
+	var mTime, size int64
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		fmt.Printf("worker %d: error os.Stat(%s): %s\n", id, path, err)
+		return FileDesc{}, err
+	} else {
+		mTime = fileInfo.ModTime().Unix()
+		size  = fileInfo.Size()
+	}
+	return FileDesc{path, mTime, size}, nil
+}
+
+func getMD5(path string) (string, error) {
 	h := md5.New()
 	file, err := os.Open(path)
 	if err != nil {
-		return FileDesc{}, err
+		return "", err
 	}
-	defer file.Close()
-
-	buf := make([]byte, 4096)
+	buf := make([]byte, 1048576)
 	for {
 		count, err := file.Read(buf)
 		if err != nil {
 			if err == io.EOF {
 				break
 			} else {
-				return FileDesc{}, err
+				return "", err
 			}
 		}
 		h.Write(buf[0:count])
@@ -45,19 +56,11 @@ func generateFileDesc(id int, path string) (FileDesc, error) {
 	// XXX can call h.checkSum?
 	hash := fmt.Sprintf("%x", h.Sum(nil))
 
-	var mTime int64
-	fileInfo, err := file.Stat()
-	if err != nil {
-		fmt.Printf("worker %d: error os.Stat(%s): %s\n", id, path, err)
-		mTime = 0
-	} else {
-		mTime = fileInfo.ModTime().Unix()
-	}
-	return FileDesc{path, mTime, hash}, nil
+	return hash, nil
 }
 
-func processFiles(id int, taskQueue <-chan string, doneQueue chan<- map[string][]FileDesc) {
-	filesByHash := make(map[string][]FileDesc)
+func processFiles(id int, taskQueue <-chan string, doneQueue chan<- map[int64][]FileDesc) {
+	filesBySize := make(map[int64][]FileDesc)
 	fmt.Printf("worker %d: starting up!\n", id)
 	for filename := range taskQueue {
 		fileDesc, err := generateFileDesc(id, filename)
@@ -65,15 +68,15 @@ func processFiles(id int, taskQueue <-chan string, doneQueue chan<- map[string][
 			fmt.Printf("worker %d: error generating entry for %s: %s\n", id, filename, err)
 			continue
 		}
-		filesByHash[fileDesc.hash] = append(filesByHash[fileDesc.hash], fileDesc)
+		filesBySize[fileDesc.size] = append(filesBySize[fileDesc.size], fileDesc)
 	}
-	fmt.Printf("worker %d: sending back %d entries\n", id, len(filesByHash))
-	doneQueue <- filesByHash
+	fmt.Printf("worker %d: sending back %d entries\n", id, len(filesBySize))
+	doneQueue <- filesBySize
 }
 
 func findDupes(dirname string, numWorkers int) map[string][]FileDesc {
 	taskQueue := make(chan string, 100)
-	doneQueue := make(chan map[string][]FileDesc)
+	doneQueue := make(chan map[int64][]FileDesc)
 
 	for i := 0; i < numWorkers; i++ {
 		go processFiles(i, taskQueue, doneQueue)
@@ -101,13 +104,31 @@ func findDupes(dirname string, numWorkers int) map[string][]FileDesc {
 	close(taskQueue)
 	//fmt.Printf("done calling filepath.Walk(%s)\n", dirname)
 
-	filesByHash := make(map[string][]FileDesc)
+	filesBySize := make(map[int64][]FileDesc)
 	for i := 0; i < numWorkers; i++ {
-		workerFilesByHash := <-doneQueue
-		for k, v := range workerFilesByHash {
-			filesByHash[k] = append(filesByHash[k], v...)
+		workerFilesBySize := <-doneQueue
+		for k, v := range workerFilesBySize {
+			filesBySize[k] = append(filesBySize[k], v...)
 		}
 	}
+
+	// walk dupes, get md5s
+	filesByHash := make(map[string][]FileDesc)
+	for _, fileDescs := range filesBySize {
+		if len(fileDescs) < 2 {
+			continue
+		}
+		for i := range fileDescs {
+			hash, err := getMD5(fileDescs[i].path)
+			if err != nil {
+				fmt.Printf("error getMD5(%s): %s\n", fileDescs[i].path, err)
+				continue
+			} else {
+				filesByHash[hash] = append(filesByHash[hash], fileDescs[i])
+			}
+		}
+	}
+
 	return filesByHash
 }
 
@@ -118,7 +139,7 @@ func printDupes(dirname string, numWorkers int) {
 		return
 	}
 
-	fmt.Printf("\nDupes found:\n")
+	fmt.Printf("\nDuplicates:\n")
 	// XXX how to iterate in sorted order
 	for hash, fileDescs := range filesByHash {
 		if len(fileDescs) < 2 {
@@ -142,7 +163,7 @@ func IsDirectory(path string) (bool, error) {
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Printf("Usage: find_dupes_merge_maps <PATH> [ <NUM_WORKERS> ]\n")
+		fmt.Printf("Usage: find_dupes <PATH> [ <NUM_WORKERS> ]\n")
 		os.Exit(1)
 	}
 
